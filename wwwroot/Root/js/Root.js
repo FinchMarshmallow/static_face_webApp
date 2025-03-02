9e2ca40e3dc11087557d777f84a73ad8d3e6b81a
Marshmallow Siskin <134213041+FinchMarshmallow@users.noreply.github.com>
2025-03-02 23:48:53 +0400

    start

---
+++ wwwroot/Root/js/Root.js
@@ -0,0 +1,8 @@
+﻿
+//          Button id = "myButton"         Event type click = GET /button-click
+document.getElementById("myButton").addEventListener("click", async () =>
+{
+	const response = await fetch('/button-click');
+	const text = await response.text();
+	document.getElementById("response").innerText = text;
+});
